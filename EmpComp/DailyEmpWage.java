9b088f86565c3a4db2c80d802bf0575aecc9cd84
lakhankumar <lakhankumar997@gmail.com>
2021-08-12 21:47:28 +0530

    daily emp wage

---
+++ EmpComp/DailyEmpWage.java
@@ -0,0 +1,20 @@
+package EmpComp;
+
+public class DailyEmpWage {
+
+	public static void main(String[] args) {
+		int IS_PRESENT = 1;
+		int EMP_WAGE_PER_HR = 20;
+		int attendance =  (int) (Math.floor(Math.random()*10)%2);
+		if (attendance == IS_PRESENT) {
+			System.out.println("employee is present");
+			int WORKING_HRS = 8;
+			int EmpWage = WORKING_HRS*EMP_WAGE_PER_HR;
+			System.out.println(EmpWage);
+		}
+		else {
+			System.out.println("employee is absent");
+		}
+	}
+
+}
